855b1f6120b527ca07cbbc00f18ffa71976504ec
Your Name <your_email@example.com>
2024-09-25 17:51:25 +0530

    MY first branch commit

---
+++ Python_day_1.py
@@ -0,0 +1,50 @@
+#print("Hello World")
+
+#conditional statements
+'''age=int(input("Enter your age"))
+
+if age>=18:
+    print("You can apply for voterID Card")
+
+else:
+    print("Not eligible")'''
+
+#set
+#adding new element in the set
+'''s1=[1,2,4,5,6,7,2,4,8,9,8,6,8,]
+s1.append(51)
+print(s1)
+s1.remove(2)
+print(s1)'''
+
+#changing tghe data type
+'''a="123456"
+print(type(a))
+print(int(a))
+print(type(a))'''
+l1=[x for x**2 in range(10)]
+print(l1)
+
+#dictionary
+l1=[x for x**2 in range(10)]
+print(l1)
+
+
+
+
+'''my_function("Saniya")
+
+def fact(n):
+  fac=1
+  for i in range (1,n+1):
+    fac=fac*i
+
+print(fac)
+fact(5)'''
+
+l1=[x for x**2 in range(10)]
+print(l1)
+
+
+l2= [x for x in range(10) if x < 5] 
+print(l2)
